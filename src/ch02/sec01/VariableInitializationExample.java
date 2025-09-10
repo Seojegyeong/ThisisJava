dc3af29ad2b47adda1492086474f3f8a7d897965
서제경 <seojk0315@naver.com>
2025-09-10 12:59:45 +0900

    first commit

---
+++ src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,12 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+    public static void main(String[] args){
+        int value=0;
+
+        int result=value+10;
+        //value 선언만 하고 초기화 안해서 오류난것
+
+        System.out.println(result);
+    }
+}
